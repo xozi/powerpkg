d35282125f6bb776ec866a69d870c649a0e64914
xozi <apexcrow@gmail.com>
2025-01-25 16:37:38 -0900

    String parser for running each

--- src/main.cu
+++ src/main.cu
@@ -2,16 +2,33 @@
 #include "power_abc.cuh"
 #include "ex1.cuh"
 #include "hw1.cuh"
-int main() {
-    //Test Example 1 (from ex1.cuh)
-    //PowerMatrix<cuFloatComplex> result = ex1_unit_test();
+#include <cstring>
 
-    //Test Homework 1 (from hw1.cuh)
-    PowerLineMatrix<cuFloatComplex> result = ex1_unit_test();
+int main(int argc, char* argv[]) {
+    // Check for specific command line arguments
+    if (argc < 2) {
+        printf("Usage: %s <test_type>\n", argv[0]);
+        return 1; // Exit with error if no arguments are provided
+    }
+
+    PowerLineMatrix<cuFloatComplex> result;
+
+    // Determine which test to run based on the argument
+    if (strcmp(argv[1], "ex1") == 0) {
+        // Test Example 1 (from ex1.cuh)
+        result = ex1_unit_test();
+    } else if (strcmp(argv[1], "hw1") == 0) {
+        // Test Homework 1 (from hw1.cuh)
+        result = hw1_unit_test(); 
+    } else {
+        printf("Error: Unknown test type '%s'. Use 'ex1' or 'hw1'.\n", argv[1]);
+        return 1; 
+    }
+
     int filewrite = write_log(result);
     if (filewrite != 0) {
         printf("Error: write_log failed with code %d\n", filewrite);
-        return filewrite; // Exit with the error code
+        return filewrite; 
     }
     return 0;
 }
